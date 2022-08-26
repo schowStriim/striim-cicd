751df79a71ffbb0225c131533a4594b7234a53bc
EC2 Default User <ec2-user@ip-172-31-5-43.us-west-2.compute.internal>
2022-08-26 20:10:42 +0000

    testing

---
+++ image-builds/ansible/files/striim_credentials.sh
@@ -0,0 +1,4 @@
+sed -i 's/WAClusterName=/WAClusterName='"$WAClusterName"'/' /opt/Striim/conf/startUp.properties
+sed -i 's/CompanyName=/CompanyName='"$CompanyName"'/' /opt/Striim/conf/startUp.properties
+sed -i 's/ProductKey=/ProductKey='"$ProductKey"'/' /opt/Striim/conf/startUp.properties
+sed -i 's/LicenceKey=/LicenceKey='"$LicenceKey"'/' /opt/Striim/conf/startUp.properties
