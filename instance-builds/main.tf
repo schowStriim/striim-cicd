5b78c1c644904a5b63732bf718aa74d5221bd699
EC2 Default User <ec2-user@ip-172-31-5-43.us-west-2.compute.internal>
2022-08-29 13:51:02 +0000

    updated

--- instance-builds/main.tf
+++ instance-builds/main.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "aws-striim-sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow incoming SSH connections (Linux)"
   }
+ ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Open port for Striim and databases"
+  }
   egress {
     from_port   = 0
     to_port     = 0
