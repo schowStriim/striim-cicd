d3126fa2d55cc1157b4bfd1dc944a5a9126bc6de
EC2 Default User <ec2-user@ip-172-31-5-43.us-west-2.compute.internal>
2022-08-30 20:18:27 +0000

    lambda code change

--- instance-builds/ec2_lambda_handler/ec2_lambda_handler.py
+++ instance-builds/ec2_lambda_handler/ec2_lambda_handler.py
@@ -4,6 +4,38 @@ import os
 region = os.environ.get('AWS_REGION')
 
 ec2 = boto3.client('ec2', region_name=region)
+rds = boto3.client('rds')
+rds_instances = []
+ec2_instances = []
+
+rds_response = rds.describe_db_instances()
+
+def rds_stop_instance(response):
+    
+    # Locate all instances that are tagged ttl.
+    for instance in rds_response["DBInstances"]:
+        
+        tags = rds.list_tags_for_resource(ResourceName=instance["DBInstanceArn"])
+                
+        for tag in tags["TagList"]:
+            if tag['Key'] == 'Auto-Start':
+                     if tag['Value'] == 'true':
+                         rds.stop_db_instance(DBInstanceIdentifier=instance["DBInstanceIdentifier"])
+                        print("Stopped RDS Instance: ", instance["DBInstanceIdentifier"])
+                    
+def rds_start_instance(response):
+    
+    # Locate all instances that are tagged ttl.
+    for instance in rds_response["DBInstances"]:
+        
+        tags = rds.list_tags_for_resource(ResourceName=instance["DBInstanceArn"])
+                
+        for tag in tags["TagList"]:
+            if tag['Key'] == 'Auto-Start':
+                     if tag['Value'] == 'true':
+                         rds.start_db_instance(DBInstanceIdentifier=instance["DBInstanceIdentifier"])
+                         print("Started RDS Instance: ", instance["DBInstanceIdentifier"])
+                         
 response = ec2.describe_instances(Filters=[
         {
             'Name': 'tag:Auto-Start',
@@ -13,22 +45,24 @@ response = ec2.describe_instances(Filters=[
         },
     ])
 
-instances = []
 
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
-        instances.append(instance["InstanceId"])
+        ec2_instances.append(instance["InstanceId"])
 
 def stop(event, context):
-    if instances:
-        ec2.stop_instances(InstanceIds=instances)
-        print('Stopped instances: ' + str(instances))
+    if ec2_instances:
+        ec2.stop_instances(InstanceIds=ec2_instances)
+        rds_stop_instance(rds_response)
+        print('Stopped EC2 instances: ' + str(ec2_instances))
     else: 
         print('No Instances with Auto-Start tag.')
         
 def start(event, context):
-    if instances:
-        ec2.start_instances(InstanceIds=instances)
-        print('Started instances: ' + str(instances))
+    if ec2_instances:
+        rds_start_instance(rds_response)
+        ec2.start_instances(InstanceIds=ec2_instances)
+        print('Started EC2 instances: ' + str(ec2_instances))
     else: 
         print('No Instances with Auto-Start tag.')
+
