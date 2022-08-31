ac4b0e3c36ff9f2caac1f1f1ba0e16a87e28946e
EC2 Default User <ec2-user@ip-172-31-5-43.us-west-2.compute.internal>
2022-08-31 17:49:09 +0000

    adding pg hba conf file

--- image-builds/ansible/files/striim_credentials.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/WAClusterName=/WAClusterName='"$WAClusterName"'/' /opt/Striim/conf/startUp.properties
-sed -i 's/CompanyName=/CompanyName='"$CompanyName"'/' /opt/Striim/conf/startUp.properties
-sed -i 's/ProductKey=/ProductKey='"$ProductKey"'/' /opt/Striim/conf/startUp.properties
-sed -i 's/LicenceKey=/LicenceKey='"$LicenceKey"'/' /opt/Striim/conf/startUp.properties
