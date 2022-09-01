be59d8e3eaf217f073723b8f5b27c44f39168cfe
schowStriim <111879867+schowStriim@users.noreply.github.com>
2022-09-01 09:04:16 -0500

    enabling server side encryption

--- instance-builds/main.tf
+++ instance-builds/main.tf
@@ -23,11 +23,14 @@ resource "aws_s3_bucket" "terraform_state"{
   versioning {
     enabled = true
   }
-  server_side_encryption_configuration{
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_bucket_encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
