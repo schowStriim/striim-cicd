46efe8732123d3493a0bf6a633189e606574e7ec
Simson Chow <simsonchow@Simsons-MBP.hitronhub.home>
2022-09-07 14:22:54 -0500

    adding bash script to install striim

---
+++ striim-install/striim-centos-install.sh
@@ -0,0 +1,13 @@
+# Install Java JDK (1.8)
+
+curl -0 -L https://striim-downloads.s3.us-west-1.amazonaws.com/jdk-8u341-linux-x64.tar.gz --output jdk-8u341-linux-x64.tar.gz
+mkdir -p /usr/lib/jvm
+tar zxvf jdk-8u341-linux-x64.tar.gz -C /usr/lib/jvm
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_341/bin/java" 1
+update-alternatives --set java /usr/lib/jvm/jdk1.8.0_341/bin/java
+
+# Install Striim
+curl -L https://striim-downloads.striim.com/Releases/4.1.0.1/striim-dbms-4.1.0.1-Linux.rpm --output striim-dbms-4.1.0.1-Linux.rpm
+curl -L https://striim-downloads.striim.com/Releases/4.1.0.1/striim-node-4.1.0.1-Linux.rpm --output striim-node-4.1.0.1-Linux.rpm
+rpm -ivh striim-dbms-4.1.0.1-Linux.rpm
+rpm -ivh striim-node-4.1.0.1-Linux.rpm
