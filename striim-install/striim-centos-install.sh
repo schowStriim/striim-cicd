4dea891121050d7f5f6bf378d7b26374fde27315
schowStriim <111879867+schowStriim@users.noreply.github.com>
2022-09-08 14:06:52 -0500

    Update striim-centos-install.sh

--- striim-install/striim-centos-install.sh
+++ striim-install/striim-centos-install.sh
@@ -8,8 +8,8 @@ update-alternatives --set java /usr/lib/jvm/jdk1.8.0_341/bin/java
 # Install Striim
 curl -L https://striim-downloads.striim.com/Releases/4.1.0.1/striim-dbms-4.1.0.1-Linux.rpm --output striim-dbms-4.1.0.1-Linux.rpm
 curl -L https://striim-downloads.striim.com/Releases/4.1.0.1/striim-node-4.1.0.1-Linux.rpm --output striim-node-4.1.0.1-Linux.rpm
-rpm -ivh striim-dbms-4.1.0.1-Linux.rpm
-rpm -ivh striim-node-4.1.0.1-Linux.rpm
+sudo rpm -ivh striim-dbms-4.1.0.1-Linux.rpm
+sudo rpm -ivh striim-node-4.1.0.1-Linux.rpm
 
 # Setup Striim's credentials
 sudo su - striim /opt/striim/bin/sksConfig.sh
