aea03d8ffb70e09b8ca829054991c39117ac1691
schowStriim <111879867+schowStriim@users.noreply.github.com>
2022-10-27 13:46:30 -0500

    Update main.tf

--- instance-builds/main.tf
+++ instance-builds/main.tf
@@ -30,6 +30,18 @@ provider "google" {
 # GCP       #
 #############
 
+resource "google_bigquery_dataset" "default" {
+  dataset_id                  = "foo"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+
 # Create a single Compute Engine instance
 resource "google_compute_instance" "default" {
   name         = "striim-server"
